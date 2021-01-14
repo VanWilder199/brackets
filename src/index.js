ca85aed828e7cc58aa22c67ff748fa3d11714155
VanWilder199 <72692962+VanWilder199@users.noreply.github.com>
2021-01-14 21:29:00 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   let bracketsS = bracketsConfig.map (element => element.join(''));
+
+   for (let i = 0; i < bracketsS.length; i++) {
+    if( str.includes(bracketsS[i]) ) {
+        str = str.replace(bracketsS[i], "");
+        i = -1;
+      }
+    }
+    return  str.length === 0 ? true : false;
+   }
+
